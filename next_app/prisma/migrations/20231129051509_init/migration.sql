96b8b06e5bb969c74bc43ed44c7f8ac4adec9eb5
Yuto Suzuki <yuto.suzuki.0403@gmail.com>
2023-11-29 19:37:44 +0900

    delete: migration files

--- next_app/prisma/migrations/20231129051509_init/migration.sql
+++ next_app/prisma/migrations/20231129051509_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "promisedTime" TIMESTAMP(3) NOT NULL,
 
@@ -9,12 +9,25 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "Attendance" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
     "attendanceTime" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Attendance_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Absence" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "reason" TEXT NOT NULL,
+    "absenceTime" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Absence_pkey" PRIMARY KEY ("id")
+);
+
 -- AddForeignKey
 ALTER TABLE "Attendance" ADD CONSTRAINT "Attendance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Absence" ADD CONSTRAINT "Absence_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
