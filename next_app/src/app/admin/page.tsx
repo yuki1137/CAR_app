a8b6f18c7265f499c86f72c4dbbd29f0ce9648fa
Leo-Ando <20s2002j@shinshu-u.ac.jp>
2023-12-16 22:23:41 +0900

    表の仮表示

--- next_app/src/app/admin/page.tsx
+++ next_app/src/app/admin/page.tsx
@@ -8,6 +8,8 @@ import { stringify } from "querystring";
 import Header from "../../components/Header";
 import { FaUserCog } from "react-icons/fa";
 import { userAgent } from "next/server";
+import convertTimeToHHMMFormat from "../../utils/convertTimeToHHMMFormat";
+import { useTable } from "react-table";
 
 type PostDataType = {
   name: string;
@@ -15,7 +17,7 @@ type PostDataType = {
 };
 
 export default function Page() {
-  const { data, isLoading } = useQuery<User[]>({
+  const { data: users, isLoading } = useQuery<User[]>({
     queryKey: ["users"],
     queryFn: async () => {
       const { data } = await axios.get("/api/users");
@@ -35,38 +37,106 @@ export default function Page() {
     mutate({ name: "Carlie", promisedTime: "2021-01-01" });
   };
 
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: (d: User) => d.name,
+      },
+      {
+        Header: "Promised Time",
+        accessor: (d: User) => convertTimeToHHMMFormat(d.promisedTime),
+      },
+      {
+        Header: "ID",
+        accessor: (d: User) => d.id,
+      },
+    ],
+    [],
+  );
+
+  const tableInstance = useTable({
+    columns,
+    data: users || [],
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  const tableStyles = {
+    borderCollapse: "collapse" as "collapse",
+    width: "100%",
+  };
+
+  const thStyles = {
+    border: "1px solid black",
+    padding: "8px",
+    textAlign: "center" as "center",
+    backgroundColor: "#f2f2f2",
+  };
+
+  const tdStyles = {
+    border: "1px solid black",
+    padding: "8px",
+  };
+
+  const tableContainerStyles = {
+    overflowX: "auto", // 横スクロールを有効にする
+  };
+
+  const idCellStyles = {
+    maxWidth: "100px" as const, // 最大幅を設定
+    overflow: "hidden" as const, // はみ出したテキストを隠す
+    textOverflow: "ellipsis" as const, // 省略記号を表示
+    whiteSpace: "nowrap" as const, // 改行を防ぐ
+  };
+
   return (
     <>
       <Header title="管理ページ" icon={<FaUserCog size={30} />} />
       <button onClick={handleAddUser}>add user</button>
       <div>isLoading: {isLoading ? "true" : "false"} </div>
       <ul>
-        {data &&
-          data.map((user) => (
+        {users &&
+          users.map((user: User) => (
             <li key={user.id}>
+              {/* && 演算子は論理AND演算を行い、左側の式（型チェック）が true の場合にのみ右側の式（値の出力）が評価される */}
               {user.name} {typeof user.promisedTime === "string" && user.promisedTime}
             </li>
           ))}
       </ul>
-      <table>
+      <table {...getTableProps()} style={tableStyles}>
         <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Promised Time</th>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()} style={thStyles} key={column.id}>
+                  {column.render("Header")}
+                </th>
+              ))}
             </tr>
+          ))}
         </thead>
-        <tbody>
-        {data?.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>{user.promisedTime}</td>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} key={row.id}>
+                {row.cells.map((cell) => (
+                  <td
+                    {...cell.getCellProps()}
+                    style={cell.column.id === "id" ? idCellStyles : tdStyles}
+                    key={cell.column.id}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                ))}
               </tr>
-            ))}
+            );
+          })}
         </tbody>
       </table>
-
     </>
   );
 }
