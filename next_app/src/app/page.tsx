df27107a829135fa8edc535d42afa804b2b00c67
taiyaki-man <uagmi20938@yahoo.co.jp>
2023-12-21 19:14:56 +0900

    test

--- next_app/src/app/page.tsx
+++ next_app/src/app/page.tsx
@@ -6,6 +6,7 @@ import { string } from "zod";
 import { stringify } from "querystring";
 import Header from "../components/Header";
 import { FaHome } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 type PostDataType = {
   name: string;
@@ -13,7 +14,8 @@ type PostDataType = {
 };
 
 export default function Page() {
-  const { data, isLoading } = useQuery<User[]>({
+  const [data, setData] = useState<User[]>([]);
+  const { isLoading } = useQuery<User[]>({
     queryKey: ["users"],
     queryFn: async () => {
       const { data } = await axios.get("/api/users");
@@ -29,15 +31,54 @@ export default function Page() {
     },
   });
 
+  const [selectedName, setSelectedName] = useState<string>(""); // 選択された名前を管理
+
   const handleAddUser = () => {
     mutate({ name: "Carlie", promisedTime: "2021-01-01" });
   };
+
+  //以下追加したコード
+
+  useEffect(() => {
+    // データを取得する非同期関数
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/users"); // 適切なエンドポイントに変更
+        setData(response.data); // 取得したデータをステートにセット
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData(); // 非同期関数を呼び出し
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // マウント時に1回だけ実行
 
   return (
     <>
       <Header title="HE研登校管理" icon={<FaHome size={30} />} />
       <button onClick={handleAddUser}>add user</button>
       <div>isLoading: {isLoading ? "true" : "false"} </div>
+
+      {/* プルダウンリスト */}
+      <label>
+        Select a name:
+        <select value={selectedName} onChange={(e) => setSelectedName(e.target.value)}>
+          {/* 初期オプション */}
+          <option value="">Select...</option>
+          {/* data から取得した名前をオプションとしてマップ */}
+          {data &&
+            data.map((user) => (
+              <option key={user.id} value={user.name}>
+                {user.name}
+              </option>
+            ))}
+        </select>
+      </label>
+      {/* 選択された名前を表示 */}
+      <div>Selected Name: {selectedName}</div>
+
       <ul>
         {data &&
           data.map((user) => (
