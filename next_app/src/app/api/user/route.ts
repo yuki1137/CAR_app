6ec7b1387e8e70ea0f4d851449f836051ab25e47
Leo-Ando <20s2002j@shinshu-u.ac.jp>
2023-12-26 13:19:51 +0900

    absencesを取得

--- next_app/src/app/api/user/route.ts
+++ next_app/src/app/api/user/route.ts
@@ -10,12 +10,16 @@ export async function GET(req: NextRequest) {
   if (!userId) {
     return NextResponse.json({ ok: false, error: "User ID is required" });
   }
+
   const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
+    where: { id: userId },
   });
-  return NextResponse.json(user);
+
+  const absences = await prisma.absence.findMany({
+    where: { userId: userId },
+    select: {reason: true, absenceTime: true} //公欠理由と公欠日のみ抽出
+  })
+  return NextResponse.json({ user, absences }); 
 }
 
 
