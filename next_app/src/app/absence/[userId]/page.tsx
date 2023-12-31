22fb7a63bc5ff63e30d511910ee10e261b25ec7f
Leo-Ando <20s2002j@shinshu-u.ac.jp>
2024-01-01 04:14:38 +0900

    ダークモード設定

--- next_app/src/app/absence/[userId]/page.tsx
+++ next_app/src/app/absence/[userId]/page.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import React from "react";
 import Header from "../../../components/Header";
-import { FaBed, FaCheck, FaTrashAlt } from "react-icons/fa";
+import { FaBed, FaCheck, FaTrashAlt, FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import DatePicker, { DateObject, Value } from "react-multi-date-picker";
 import Button from "../../../components/Button";
 import { useTable, useSortBy, usePagination, Row, IdType, Column } from "react-table";
@@ -22,7 +22,6 @@ const Page = ({ params }: { params: { userId: string } }) => {
   const [absenceReason, setAbsenceReason] = useState("公欠理由を記述（例：帰省）");
   const [isButtonClicked, setIsButtonClicked] = useState(false);
 
-  
   const handleAbsenceReasonChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     //event.target.value で入力フィールドの現在の値を取得
     setAbsenceReason(event.target.value);
@@ -68,6 +67,22 @@ const Page = ({ params }: { params: { userId: string } }) => {
     setAbsenceReason("");
     setIsButtonClicked(false);
   };
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  // ダークモードかどうかを判定するための useEffect
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setIsDarkMode(mediaQuery.matches);
+
+    // メディアクエリの変更を監視する
+    const handler = (e: MediaQueryListEvent) => {
+      setIsDarkMode(e.matches);
+    };
+    mediaQuery.addListener(handler);
+
+    // コンポーネントのアンマウント時にリスナーを削除
+    return () => mediaQuery.removeListener(handler);
+  }, []);
 
   ////ここからはテーブルの設定////
 
@@ -117,19 +132,15 @@ const Page = ({ params }: { params: { userId: string } }) => {
     }
   };
 
-  const [pageIndex] = useState(0); //現在のpageIndex（ページ番号）を作成,初期値を0にしページネーションにおいて最初のページを指し、ページ番号のカウントが0から始まるようにする
-  const pageSize = 10; //一度に表示されるデータの数、ここでは1ページあたりのアイテム数が10個
-  const [tableData, setTableData] = useState<Absence[]>([]); //tableDataは表に表示されるデータ、User型のオブジェクトの配列で、初期値は空
-
-  // useEffectは関数の実行タイミングをReactのレンダリング後まで遅らせるhook
-  // useEffect(() => { A }, [B]) の場合Bが変更されたらAを実行する
+  const [pageIndex] = useState(0);
+  const pageSize = 10;
+  const [tableData, setTableData] = useState<Absence[]>([]);
 
   useEffect(() => {
     if (absences) {
-      //users変数が存在すれば（nullやundefinedでなければ）setTableDataを実行する
       setTableData(absences);
     }
-  }, [absences]); //usersが変更されたときにsetTableDataを実行する
+  }, [absences]);
 
   // カスタムソート関数の定義
   const dateSort = (rowA: Row<Absence>, rowB: Row<Absence>, columnId: IdType<Absence>) => {
@@ -141,7 +152,7 @@ const Page = ({ params }: { params: { userId: string } }) => {
   const columns: Column<Absence>[] = React.useMemo(
     () => [
       {
-        Header: "操作",
+        Header: "",
         id: "actions", // `accessor` の代わりに `id` を使用
         Cell: ({ row }: { row: Row<Absence> }) => {
           // 日付の比較
@@ -197,23 +208,14 @@ const Page = ({ params }: { params: { userId: string } }) => {
   //tableInstanceオブジェクトからテーブルのレンダリングと操作に必要な一連のプロパティと関数を抽出
   const {
     getTableProps,
-    //テーブル要素（<table>タグ）に適用すべきプロパティとイベントハンドラを提供、react-tableが内部的にテーブルの状態と相互作用を管理できるようになる
     getTableBodyProps,
-    //テーブル本体（<tbody>タグ）に適用するプロパティとイベントハンドラを提供
     headerGroups,
-    //テーブルのヘッダー行（<thead>内の<tr>）を表すオブジェクトの配列。各ヘッダーグループは、一つ以上のヘッダー行に関する情報を含んでいる
     page,
-    //現在のページに表示される行（データ）の配列。ページネーションが有効なので現在のページに対応するデータのみを含む
     nextPage,
-    //テーブルの次のページに移動するための関数
     previousPage,
-    //テーブルの前のページに移動するための関数
     canNextPage,
-    //、次のページへの移動が可能かどうかを示すブール値。次のページボタンの有効/無効状態を制御
     canPreviousPage,
-    //前のページへの移動が可能かどうかを示すブール値
     prepareRow,
-    //各行（<tr>）をレンダリングする前に呼び出す必要がある関数。行に必要なプロパティやイベントハンドラを準備する
     state: { pageIndex: currentPageIndex },
     rows,
   } = tableInstance;
@@ -225,39 +227,37 @@ const Page = ({ params }: { params: { userId: string } }) => {
 
   //テーブル全体のスタイル
   const tableStyles = {
-    borderCollapse: "collapse" as "collapse", //隣接するセルの境界線を1つにまとめる,as "collapse"でこの値が文字列型であることを強調
-    width: "100%", //テーブルが親要素の全幅をうめる。テーブルは利用可能な全幅を使用する
+    borderCollapse: "collapse" as "collapse",
+    width: "100%",
   };
 
   //テーブルのヘッダー行のスタイル
   const thStyles = {
-    border: "1px solid", //要素の境界線のスタイル。solidは実線という意味
-    padding: "8px", //要素の内側の余白
-    textAlign: "center" as "center", //テキストの水平方向の配置は中央
-    // backgroundColor: "white", //要素の背景色
+    border: "1px solid",
+    padding: "8px",
+    textAlign: "center" as "center",
   };
 
   //テーブルのデータセルのスタイル
   const tdStyles = {
-    border: "1px solid", //要素の境界線のスタイル
-    padding: "8px", //要素の内側の余白
-    textAlign: "center" as "center", //テキストの水平方向の配置は中央
-    overflow: "auto", //コンテンツがセルのサイズを超えた場合の挙動を指定,"auto"で必要に応じてスクロールバーが表示される
+    border: "1px solid",
+    padding: "8px",
+    textAlign: "center" as "center",
   };
 
   //テーブルを包むコンテナ(テーブルを包含するHTML要素)のスタイル
   const tableContainerStyles = {
-    overflowX: "auto" as const, // テーブルが水平方向のコンテナのサイズを超えた場合の挙動。"auto"なので必要に応じて水平方向のスクロールバーが表示される
-    width: "80%", // コンテナの幅を80%に設定
-    margin: "0 auto", // marginプロパティは、コンテナの外側の余白を設定。"0 auto"は、上下の余白を0にし、左右の余白を自動調整する
+    overflowX: "auto" as const,
+    width: "80%",
+    margin: "0 auto",
   };
 
   //Reasonセルのスタイル
   const idCellStyles = {
-    maxWidth: "100px" as const, // セルの最大幅を100ピクセルに設定
-    overflow: "auto" as const, // セル内の文字がセルのサイズを超えた場合に、スクロールバーが自動的に表示される
-    whiteSpace: "nowrap" as const, // nowrapはテキストを強制的に1行に保持し 改行を防ぐ
-    border: "1px solid" as const, //要素の境界線のスタイル
+    maxWidth: "100px" as const,
+    overflow: "auto" as const,
+    whiteSpace: "nowrap" as const,
+    border: "1px solid" as const,
   };
 
   const getTdStyles = (isGray: boolean) => {
@@ -267,51 +267,53 @@ const Page = ({ params }: { params: { userId: string } }) => {
       textAlign: "center" as "center",
       overflow: "auto",
     };
-  
-    // ダークモード対応のスタイルを追加
-    const darkModeStyles = {
-      '@media (prefers-color-scheme: dark)': {
-        color: 'white', // ダークモードでは白色
-      },
-      color: isGray ? 'gray' : 'initial', // 通常モードでは既存のロジックを使用
-    };
-  
-    return { ...baseStyles, ...darkModeStyles };
+
+    // ダークモードかライトモードに基づいて動的にテキストカラーを設定
+    const colorStyles = isDarkMode
+      ? { color: isGray ? "gray" : "white" }
+      : { color: isGray ? "gray" : "black" };
+
+    return { ...baseStyles, ...colorStyles };
   };
-  
+
   if (!userId) {
     return <div>NO DATA</div>;
   }
   return (
     <div>
       <Header title="公欠の登録" icon={<FaBed size={30} />} userId={userId} />
-      <h2 className="text-xl  text-center  my-8">いつ公欠しますか？</h2>
-      <div className="text-xl  justify-center text-center my-4">
+      <h2 className="text-xl  text-center  my-6">いつ公欠しますか？</h2>
+      <div className="text-xl  justify-center text-center ">
         <DatePicker
           multiple={true}
           value={absenceDate}
           onChange={setAbsenceDate}
           minDate={tomorrow}
+          className="my-4"
         />
         <input
           type="text"
           // value="公欠理由を記述（例：帰省"
-          className="h-20 justify-center border-2 border-gray-600 p-2 rounded-md my-4 text-sm"
+          className="h-20 justify-center border-2 border-gray-600 p-2 rounded-md my-3 text-sm"
           value={absenceReason}
           onChange={handleAbsenceReasonChange}
         />
       </div>
-      <div className="flex justify-center">
-        <Button onClick={handleClick} isDisabled={isButtonDisabled} size="medium" className="my-2">
+      <div className="flex justify-center" style={{ marginBottom: "40px" }}>
+        <Button onClick={handleClick} isDisabled={isButtonDisabled} size="medium">
           登録
         </Button>
       </div>
-      <h2 className="text-xl  text-center  my-8">予約一覧</h2>
+      <h2 className="text-xl  text-center  my-4">予約一覧</h2>
 
       {/* ///ここからはテーブルの表示/// */}
       {isPopupVisible && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-          <div className="bg-white p-6 rounded shadow-lg">
+        <div
+          className={`fixed inset-0 flex items-center justify-center ${
+            isDarkMode ? "bg-black" : "bg-gray-500"
+          } bg-opacity-50`}
+        >
+          <div className={`p-6 rounded shadow-lg ${isDarkMode ? "bg-dark-mode-bg" : "bg-white"}`}>
             <p className="text-center mb-4">本当に削除しますか？</p>
             <div className="flex justify-center space-x-4">
               <button
@@ -330,19 +332,13 @@ const Page = ({ params }: { params: { userId: string } }) => {
           </div>
         </div>
       )}
+
       <div style={tableContainerStyles}>
-        {/* getTableProps 関数が返すオブジェクト内のすべてのプロパティと値が、<table> タグに個別のプロパティとして適用 */}
         <table {...getTableProps()} style={tableStyles}>
           <thead>
-            {/* ヘッダー行を作成、.map()メゾットでheaderGroups配列の中身を展開し、各ヘッダーグループに対して<tr>タグを作成 */}
-            {/* indexは何行目のヘッダーかを表し、各要素を一意に識別するために使用される */}
             {headerGroups.map((headerGroup, index) => (
               <tr {...headerGroup.getHeaderGroupProps()} key={index}>
-                {/* ヘッダー行の各セルを作成 */}
-                {/* .map()メゾットでheaderGroup.headers配列の中身を展開し、各ヘッダーに対して<th>タグを作成 */}
-                {/* columはヘッダー行内の個々の列を表し、各要素を一意に識別するために使用される */}
                 {headerGroup.headers.map((column) => (
-                  // ヘッダーセルに必要なプロパティを展開
                   <th {...column.getHeaderProps()} style={thStyles} key={column.id}>
                     {column.render("Header")}
                   </th>
@@ -390,6 +386,29 @@ const Page = ({ params }: { params: { userId: string } }) => {
             })}
           </tbody>
         </table>
+        <div className="flex justify-end items-center px-7 my-1">
+          <div>
+            {rows.length > 0
+              ? `${currentPageIndex * pageSize + 1}
+               -${Math.min((currentPageIndex + 1) * pageSize, rows.length)} 
+               / ${rows.length} 件`
+              : "ユーザーデータがありません"}
+          </div>
+          <button
+            onClick={() => previousPage()}
+            disabled={!canPreviousPage}
+            className={`px-1 py-2 ${!canPreviousPage ? "text-gray-400" : ""}`}
+          >
+            <FaAngleLeft size={25} />
+          </button>
+          <button
+            onClick={() => nextPage()}
+            disabled={!canNextPage}
+            className={`px-2 py-2 ${!canNextPage ? "text-gray-400" : ""}`}
+          >
+            <FaAngleRight size={25} />
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -397,4 +416,4 @@ const Page = ({ params }: { params: { userId: string } }) => {
 
 export default Page;
 
-//このtypscript,next.js14
+
